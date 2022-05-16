c7e5fcc9e5fae8cb8089c561e1a5ffd8d7c32a78
OkamotoAi <okamotoai0217@icloud.com>
2022-05-16 16:50:43 +0900

    first commit

---
+++ FizzBuzz.java
@@ -0,0 +1,19 @@
+class Main {
+
+  public static void main(String args[]) {
+    for (int i = 1; i <= 100; i++) {
+      String str = "";
+
+      // ここから記述
+      if(i % 3 == 0) str += "Fizz";
+      if(i % 5 == 0) str += "Buzz";
+      
+      if(str.isEmpty()) str = String.valueOf(i);
+
+
+      // ここまで記述
+
+      System.out.println(str);
+    }
+  }
+}
